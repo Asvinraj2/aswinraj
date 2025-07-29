8edfd70cb1d22b9f2b4b1382fc801d3123d498c7
Asvinraj2 <aswinraj2002.as@gmail.com>
2025-07-29 22:53:54 +0530

    updated the feature.jsx

--- src/components/features/Features.jsx
+++ src/components/features/Features.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import Title from "../layouts/Title";
 import Card from "./Card";
-import { featuresData } from "../../data/Data.js";
+import { featuresData } from "../../data/Data";
 
 const Features = () => {
   return (
